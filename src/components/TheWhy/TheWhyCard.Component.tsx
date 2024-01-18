2aa33f907444dc6c09e9f1a7443d1ecf00a561aa
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-01-18 17:44:50 +0530

    The Why Section updated for Mobile Screen

--- src/components/TheWhy/TheWhyCard.Component.tsx
+++ src/components/TheWhy/TheWhyCard.Component.tsx
@@ -22,21 +22,21 @@ const TheWhyCard: React.FC<TheyWhyCardProps> = ({
   return (
     <div
       className={cn(
-        "bg-white flex flex-col justify-center items-center py-4 px-2 lg:p-8 rounded-xl w-full shadow",
+        "bg-white flex flex-col justify-center items-center p-2 py-3 gap-1 lg:p-8 rounded-lg w-full shadow",
         className
       )}
     >
       {/* Icon */}
-      <div className="sm:h-16 sm:w-16 md:h-20 md:w-20 bg-white-2 rounded-full flex justify-center items-center text-base md:text-xl lg:text-3xl text-blue-1">
+      <div className="sm:h-16 sm:w-16 md:h-20 md:w-20 bg-white-2 rounded-full flex justify-center items-center text-2xl lg:text-3xl text-blue-1">
         {getIcon(icon)}
       </div>
 
       {/* Title */}
-      <div className="mt-6 text-center text-[0.9rem] lg:text-lg font-semibold">
+      <div className="mt-1 text-center text-[0.9rem] lg:text-lg font-semibold">
         {title}
       </div>
       {/* Subtible */}
-      <p className="mt-4 text-center text-gray-1 text-[0.7rem] lg:text-base font-semibold">
+      <p className="text-center text-gray-1 text-[0.7rem] lg:text-base font-semibold">
         {subtitle}
       </p>
     </div>
